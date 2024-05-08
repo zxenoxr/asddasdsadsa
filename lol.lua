9d7f2ff8e37ca7713daff98ca7fd95b2eafe54a1
zxenoxr <162846974+zxenoxr@users.noreply.github.com>
2024-05-08 17:28:43 +0100

    Create lol.lua

---
+++ lol.lua
@@ -0,0 +1,380 @@
+--[[
+
+		Gui2Lua™
+		10zOfficial
+		Version 1.0.0
+
+]]
+
+
+-- Instances
+
+local STFOGUIESZ = Instance.new("ScreenGui")
+local Main = Instance.new("Frame")
+local CumFast = Instance.new("TextButton")
+local UICorner = Instance.new("UICorner")
+local PenisScript = Instance.new("TextButton")
+local UICorner_2 = Instance.new("UICorner")
+local Fov = Instance.new("TextButton")
+local UICorner_3 = Instance.new("UICorner")
+local TextButton = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
+local TextButton_2 = Instance.new("TextButton")
+local UICorner_5 = Instance.new("UICorner")
+local TextButton_3 = Instance.new("TextButton")
+local UICorner_6 = Instance.new("UICorner")
+local TextButton_4 = Instance.new("TextButton")
+local UICorner_7 = Instance.new("UICorner")
+local TextButton_5 = Instance.new("TextButton")
+local UICorner_8 = Instance.new("UICorner")
+local Killall = Instance.new("TextButton")
+local UICorner_9 = Instance.new("UICorner")
+local UICorner_10 = Instance.new("UICorner")
+local Title = Instance.new("TextLabel")
+local UICorner_11 = Instance.new("UICorner")
+
+-- Properties
+
+STFOGUIESZ.Name = "STFO-GUI-ESZ"
+STFOGUIESZ.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+STFOGUIESZ.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Main.Name = "Main"
+Main.Parent = STFOGUIESZ
+Main.BackgroundColor3 = Color3.new(0, 0, 0)
+Main.BorderColor3 = Color3.new(0, 0, 0)
+Main.BorderSizePixel = 0
+Main.Position = UDim2.new(0.334672451, 0, 0.336647034, 0)
+Main.Size = UDim2.new(0, 485, 0, 260)
+
+CumFast.Name = "CumFast"
+CumFast.Parent = Main
+CumFast.BackgroundColor3 = Color3.new(1, 0, 0)
+CumFast.BorderColor3 = Color3.new(0, 0, 0)
+CumFast.BorderSizePixel = 0
+CumFast.Position = UDim2.new(0.0299999751, 0, 0.275897563, 0)
+CumFast.Size = UDim2.new(0, 100, 0, 48)
+CumFast.Font = Enum.Font.GothamBold
+CumFast.Text = "Become Fast"
+CumFast.TextColor3 = Color3.new(0, 0, 0)
+CumFast.TextScaled = true
+CumFast.TextSize = 14
+CumFast.TextWrapped = true
+
+UICorner.Parent = CumFast
+UICorner.CornerRadius = UDim.new(0.300000012, 0)
+
+PenisScript.Name = "PenisScript"
+PenisScript.Parent = Main
+PenisScript.BackgroundColor3 = Color3.new(1, 0, 0)
+PenisScript.BorderColor3 = Color3.new(0, 0, 0)
+PenisScript.BorderSizePixel = 0
+PenisScript.Position = UDim2.new(0.400000006, 0, 0.275897563, 0)
+PenisScript.Size = UDim2.new(0, 100, 0, 48)
+PenisScript.Font = Enum.Font.GothamBold
+PenisScript.Text = "Get a penis (broken)"
+PenisScript.TextColor3 = Color3.new(0, 0, 0)
+PenisScript.TextScaled = true
+PenisScript.TextSize = 14
+PenisScript.TextWrapped = true
+
+UICorner_2.Parent = PenisScript
+UICorner_2.CornerRadius = UDim.new(0.300000012, 0)
+
+Fov.Name = "Fov"
+Fov.Parent = Main
+Fov.BackgroundColor3 = Color3.new(1, 0, 0)
+Fov.BorderColor3 = Color3.new(0, 0, 0)
+Fov.BorderSizePixel = 0
+Fov.Position = UDim2.new(0.775999963, 0, 0.275897563, 0)
+Fov.Size = UDim2.new(0, 100, 0, 48)
+Fov.Font = Enum.Font.GothamBold
+Fov.Text = "Fov Boost"
+Fov.TextColor3 = Color3.new(0, 0, 0)
+Fov.TextScaled = true
+Fov.TextSize = 14
+Fov.TextWrapped = true
+
+UICorner_3.Parent = Fov
+UICorner_3.CornerRadius = UDim.new(0.300000012, 0)
+
+TextButton.Parent = Main
+TextButton.BackgroundColor3 = Color3.new(1, 0, 0)
+TextButton.BorderColor3 = Color3.new(0, 0, 0)
+TextButton.BorderSizePixel = 0
+TextButton.Position = UDim2.new(0.400000006, 0, 0.517808795, 0)
+TextButton.Size = UDim2.new(0, 100, 0, 48)
+TextButton.Font = Enum.Font.GothamBold
+TextButton.Text = "HUGE Fov Boost"
+TextButton.TextColor3 = Color3.new(0, 0, 0)
+TextButton.TextScaled = true
+TextButton.TextSize = 14
+TextButton.TextWrapped = true
+
+UICorner_4.Parent = TextButton
+UICorner_4.CornerRadius = UDim.new(0.300000012, 0)
+
+TextButton_2.Parent = Main
+TextButton_2.BackgroundColor3 = Color3.new(1, 0, 0)
+TextButton_2.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_2.BorderSizePixel = 0
+TextButton_2.Position = UDim2.new(0.775999963, 0, 0.517808795, 0)
+TextButton_2.Size = UDim2.new(0, 100, 0, 48)
+TextButton_2.Font = Enum.Font.GothamBold
+TextButton_2.Text = "GlitchSpeed (Bannable)"
+TextButton_2.TextColor3 = Color3.new(0, 0, 0)
+TextButton_2.TextScaled = true
+TextButton_2.TextSize = 14
+TextButton_2.TextWrapped = true
+
+UICorner_5.Parent = TextButton_2
+UICorner_5.CornerRadius = UDim.new(0.300000012, 0)
+
+TextButton_3.Parent = Main
+TextButton_3.BackgroundColor3 = Color3.new(1, 0, 0)
+TextButton_3.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_3.BorderSizePixel = 0
+TextButton_3.Position = UDim2.new(0.0299999751, 0, 0.517808795, 0)
+TextButton_3.Size = UDim2.new(0, 100, 0, 48)
+TextButton_3.Font = Enum.Font.GothamBold
+TextButton_3.Text = "BIG Fov Boost"
+TextButton_3.TextColor3 = Color3.new(0, 0, 0)
+TextButton_3.TextScaled = true
+TextButton_3.TextSize = 14
+TextButton_3.TextWrapped = true
+
+UICorner_6.Parent = TextButton_3
+UICorner_6.CornerRadius = UDim.new(0.300000012, 0)
+
+TextButton_4.Parent = Main
+TextButton_4.BackgroundColor3 = Color3.new(1, 0, 0)
+TextButton_4.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_4.BorderSizePixel = 0
+TextButton_4.Position = UDim2.new(0.400000006, 0, 0.768330872, 0)
+TextButton_4.Size = UDim2.new(0, 100, 0, 48)
+TextButton_4.Font = Enum.Font.GothamBold
+TextButton_4.Text = "Normal Speed"
+TextButton_4.TextColor3 = Color3.new(0, 0, 0)
+TextButton_4.TextScaled = true
+TextButton_4.TextSize = 14
+TextButton_4.TextWrapped = true
+
+UICorner_7.Parent = TextButton_4
+UICorner_7.CornerRadius = UDim.new(0.300000012, 0)
+
+TextButton_5.Parent = Main
+TextButton_5.BackgroundColor3 = Color3.new(1, 0, 0)
+TextButton_5.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_5.BorderSizePixel = 0
+TextButton_5.Position = UDim2.new(0.775999963, 0, 0.766240895, 0)
+TextButton_5.Size = UDim2.new(0, 100, 0, 48)
+TextButton_5.Font = Enum.Font.GothamBold
+TextButton_5.Text = "Infinite Yeild"
+TextButton_5.TextColor3 = Color3.new(0, 0, 0)
+TextButton_5.TextScaled = true
+TextButton_5.TextSize = 14
+TextButton_5.TextWrapped = true
+
+UICorner_8.Parent = TextButton_5
+UICorner_8.CornerRadius = UDim.new(0.300000012, 0)
+
+Killall.Name = "Killall"
+Killall.Parent = Main
+Killall.BackgroundColor3 = Color3.new(1, 0, 0)
+Killall.BorderColor3 = Color3.new(0, 0, 0)
+Killall.BorderSizePixel = 0
+Killall.Position = UDim2.new(0.0299999751, 0, 0.770002604, 0)
+Killall.Size = UDim2.new(0, 100, 0, 48)
+Killall.Font = Enum.Font.GothamBold
+Killall.Text = "Kill Someone"
+Killall.TextColor3 = Color3.new(0, 0, 0)
+Killall.TextScaled = true
+Killall.TextSize = 14
+Killall.TextWrapped = true
+
+UICorner_9.Parent = Killall
+UICorner_9.CornerRadius = UDim.new(0.300000012, 0)
+
+UICorner_10.Parent = Main
+
+Title.Name = "Title"
+Title.Parent = Main
+Title.BackgroundColor3 = Color3.new(1, 0, 0)
+Title.BorderColor3 = Color3.new(0, 0, 0)
+Title.BorderSizePixel = 0
+Title.Position = UDim2.new(-0.0162894651, 0, 0.103846155, 0)
+Title.Size = UDim2.new(0, 499, 0, 37)
+Title.Font = Enum.Font.GothamBold
+Title.Text = "STEAL TIMES FROM OTHERS"
+Title.TextColor3 = Color3.new(0, 0, 0)
+Title.TextScaled = true
+Title.TextSize = 14
+Title.TextWrapped = true
+
+UICorner_11.Parent = Title
+UICorner_11.CornerRadius = UDim.new(0.300000012, 0)
+
+-- Scripts
+
+local function SBVJHR_fake_script() -- CumFast.LocalScript 
+	local script = Instance.new('LocalScript', CumFast)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		local speedBoost = 22
+		local function activateSpeedBoost()
+			local player = game.Players.LocalPlayer
+			local humanoid = player.Character and player.Character:FindFirstChildOfClass("Humanoid")
+			if humanoid then
+				humanoid.WalkSpeed = humanoid.WalkSpeed + speedBoost
+			end
+		end
+		activateSpeedBoost()
+	end)
+	
+end
+coroutine.wrap(SBVJHR_fake_script)()
+local function SAOO_fake_script() -- Fov.LocalScript 
+	local script = Instance.new('LocalScript', Fov)
+
+	
+	script.Parent.MouseButton1Click:Connect(function()
+		local player = game.Players.LocalPlayer
+		local camera = game.Workspace.CurrentCamera
+		camera.FieldOfView = 200
+		local player = game.Players.LocalPlayer
+		local camera = game.Workspace.CurrentCamera
+		camera.FieldOfView = 200
+	end)
+	
+end
+coroutine.wrap(SAOO_fake_script)()
+local function HWEFG_fake_script() -- TextButton.LocalScript 
+	local script = Instance.new('LocalScript', TextButton)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		local player = game.Players.LocalPlayer
+		local camera = game.Workspace.CurrentCamera
+		camera.FieldOfView = 900
+		local player = game.Players.LocalPlayer
+		local camera = game.Workspace.CurrentCamera
+		camera.FieldOfView = 900
+	end)
+	
+end
+coroutine.wrap(HWEFG_fake_script)()
+local function NNNPCMP_fake_script() -- TextButton_2.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_2)
+
+	
+	script.Parent.MouseButton1Click:Connect(function()
+		game.Players.LocalPlayer.Character.Humanoid.Walkspeed = 25
+	end)
+end
+coroutine.wrap(NNNPCMP_fake_script)()
+local function QTTZ_fake_script() -- TextButton_3.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_3)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		local player = game.Players.LocalPlayer
+		local camera = game.Workspace.CurrentCamera
+		camera.FieldOfView = 500
+		local player = game.Players.LocalPlayer
+		local camera = game.Workspace.CurrentCamera
+		camera.FieldOfView = 500
+	end)
+	
+end
+coroutine.wrap(QTTZ_fake_script)()
+local function YUPWBAP_fake_script() -- TextButton_4.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_4)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		local speedBoost = 16
+		local function activateSpeedBoost()
+			local player = game.Players.LocalPlayer
+			local humanoid = player.Character and player.Character:FindFirstChildOfClass("Humanoid")
+			if humanoid then
+				humanoid.WalkSpeed = speedBoost
+			end
+		end
+	
+		activateSpeedBoost()
+	end)
+	
+end
+coroutine.wrap(YUPWBAP_fake_script)()
+local function JWMGFEW_fake_script() -- TextButton_5.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_5)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+	end)
+	
+end
+coroutine.wrap(JWMGFEW_fake_script)()
+local function EKGXFRI_fake_script() -- Killall.LocalScript 
+	local script = Instance.new('LocalScript', Killall)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		local function teleportToRandomPlayer()
+			local players = game.Players:GetPlayers()
+			if #players > 0 then
+				local randomIndex = math.random(1, #players)
+				local targetPlayer = players[randomIndex]
+				game.Players.LocalPlayer.Character:MoveTo(targetPlayer.Character.HumanoidRootPart.Position)
+			else
+				warn("No players found!")
+			end
+		end
+		local function performClick()
+			local input = game:GetService("UserInputService")
+			input.MouseButton1Down:Fire()
+			input.MouseButton1Up:Fire()
+		end
+	
+		teleportToRandomPlayer()
+		performClick()
+		loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+	end)
+	
+end
+coroutine.wrap(EKGXFRI_fake_script)()
+local function FQXDS_fake_script() -- Main.Grab 
+	local script = Instance.new('LocalScript', Main)
+
+	local UIS = game:GetService('UserInputService')
+	local frame = script.Parent
+	local dragToggle = nil
+	local dragSpeed = 0.25
+	local dragStart = nil
+	local startPos = nil
+	
+	local function updateInput(input)
+		local delta = input.Position - dragStart
+		local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
+			startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+		game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
+	end
+	
+	frame.InputBegan:Connect(function(input)
+		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
+			dragToggle = true
+			dragStart = input.Position
+			startPos = frame.Position
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragToggle = false
+				end
+			end)
+		end
+	end)
+	
+	UIS.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			if dragToggle then
+				updateInput(input)
+			end
+		end
+	end)
+end
+coroutine.wrap(FQXDS_fake_script)()
